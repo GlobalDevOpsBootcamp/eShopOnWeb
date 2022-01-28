642c00cc498064ad0222db48aa71bb256983726f
Erik Oppedijk <eoppedijk@xpirit.com>
2022-01-28 09:35:58 +0100

    Add Functional test for Details page

---
+++ tests/FunctionalTests/Web/Pages/DetailPageOnGet.cs
@@ -0,0 +1,29 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.eShopWeb.FunctionalTests.Web;
+using Xunit;
+
+namespace Microsoft.eShopWeb.FunctionalTests.WebRazorPages;
+
+[Collection("Sequential")]
+public class DetailPageOnGet : IClassFixture<WebTestFixture>
+{
+    public DetailPageOnGet(WebTestFixture factory)
+    {
+        Client = factory.CreateClient();
+    }
+
+    public HttpClient Client { get; }
+
+    [Fact]
+    public async Task ReturnsHomePageWithProductListing()
+    {
+        // Arrange & Act
+        var response = await Client.GetAsync("/Product/18");
+        response.EnsureSuccessStatusCode();
+        var stringResponse = await response.Content.ReadAsStringAsync();
+
+        // Assert
+        Assert.Contains("Synthetic Engine Oil", stringResponse);
+    }
+}
